57c0663a6df351138a8c5e4c7dac81c552e279bc
Evan Kirkiles <evkirkiles@gmail.com>
2019-01-18 17:55:37 -0500

    Fixed results.

--- app/analytics.js
+++ app/analytics.js
@@ -135,7 +135,7 @@ class SQLInterface {
 			// If there was an error, return
 			if (err) { console.log(err); return }
 			// Otherwise return a boolean of the availability
-			if (results.length > 1) {
+			if (results.length > 0) {
 				callback(VAULT.deals.referrals.active && results[0]['usedreferral'] == 0 && results[0]['referredby'] != '-', results[0]['referredby'])
 			} else {
 				callback(false, 'asdasd')
